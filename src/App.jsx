4c96c68758cfba84141841b43acaeb100a9309e6
jiwkovva19 <yankowadelyana@gmail.com>
2024-08-24 13:57:51 +0300

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 
 import Home from './pages/Home.jsx';
-import About from './pages/About.jsx';
+/*import About from './pages/About.jsx';*/
 import Contacts  from './pages/Contacts.jsx';
 
 
@@ -31,7 +31,7 @@ function App()
      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        
         <Route path="/contacts" element={<Contacts />} />
 
 
