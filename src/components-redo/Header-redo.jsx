7b8fb8aec361a9d7824dc653f66caddf4a584c22
jiwkovva19 <yankowadelyana@gmail.com>
2024-10-09 20:20:46 +0300

    permanent side menu bar initial commit

--- src/components-redo/Header-redo.jsx
+++ src/components-redo/Header-redo.jsx
@@ -6,22 +6,23 @@ import logo2 from '../assets/seai-blue-logo.png';
 
 const Header = () => {
   const [navOpen, setNavOpen] = useState(false);
+  const [collapsed, setCollapsed] = useState(true); // Add a state for collapsed sidebar
   const { isAuthenticated, logout } = useContext(AuthContext);
   const location = useLocation();
 
-  const openNav = () => setNavOpen(true);
-  const closeNav = () => setNavOpen(false);
+  const toggleNav = () => setNavOpen(!navOpen);
+  const toggleCollapse = () => setCollapsed(!collapsed); // Toggle collapsed state
 
   useEffect(() => {
     // Close the side menu if the user navigates to a different page
-    closeNav();
+    setNavOpen(false);
   }, [location]);
 
   return (
     <>
       <nav className="header">
         <ul className="nav-links">
-          <li className="nav-button" onClick={openNav}>
+          <li className="nav-button" onClick={toggleNav}>
             <i className="fa-solid fa-bars menu-icon"></i>
           </li>
           <li className="logo-container">
@@ -33,35 +34,60 @@ const Header = () => {
       </nav>
 
       {/* Side navigation */}
-      <div className={`side-nav ${navOpen ? 'open' : ''}`}>
-        <button className="closebtn" onClick={closeNav}>
-          <i className="fa-solid fa-x"></i>
+      <div className={`side-nav ${navOpen ? 'open' : ''} ${collapsed ? 'collapsed' : ''}`}>
+        <button className="collapsebtn" onClick={toggleCollapse}>
+          <i className={`fa-solid ${collapsed ? 'fa-arrow-right' : 'fa-arrow-left'}`}></i>
         </button>
-        <Link to="/documents" className="side-nav-link">Smart Scanner</Link>
-        <Link to="/profile" className="side-nav-link">Profile</Link>
-        <Link to="/documents" className="side-nav-link">Documents</Link>
-        <Link to="/voyages" className="side-nav-link">Voyages</Link>
-        <Link to="#" className="side-nav-link">Booking</Link>
-        <Link to="#" className="side-nav-link">Chat</Link>
-        <Link to="#" className="side-nav-link">Events</Link>
-        <Link to="#" className="side-nav-link">Maritime Administration</Link>
-        <Link to="#" className="side-nav-link">Information</Link>
+
+        {/* Sidebar Links */}
+        <Link to="/documents" className="side-nav-link">
+          <i className="fa-solid fa-file-alt"></i> {!collapsed && 'Smart Scanner'}
+        </Link>
+        <Link to="/profile" className="side-nav-link">
+          <i className="fa-solid fa-user"></i> {!collapsed && 'Profile'}
+        </Link>
+        <Link to="/documents" className="side-nav-link">
+          <i className="fa-solid fa-folder"></i> {!collapsed && 'Documents'}
+        </Link>
+        <Link to="/voyages" className="side-nav-link">
+          <i className="fa-solid fa-ship"></i> {!collapsed && 'Voyages'}
+        </Link>
+        <Link to="#" className="side-nav-link">
+          <i className="fa-solid fa-calendar-check"></i> {!collapsed && 'Booking'}
+        </Link>
+        <Link to="#" className="side-nav-link">
+          <i className="fa-solid fa-comments"></i> {!collapsed && 'Chat'}
+        </Link>
+        <Link to="#" className="side-nav-link">
+          <i className="fa-solid fa-bullhorn"></i> {!collapsed && 'Events'}
+        </Link>
+        <Link to="#" className="side-nav-link">
+          <i className="fa-solid fa-briefcase"></i> {!collapsed && 'Maritime Administration'}
+        </Link>
+        <Link to="#" className="side-nav-link">
+          <i className="fa-solid fa-info-circle"></i> {!collapsed && 'Information'}
+        </Link>
+
         <div className="auth-links">
           {isAuthenticated ? (
             <button className="side-nav-link" onClick={logout}>
-              Logout
+              <i className="fa-solid fa-sign-out-alt"></i> {!collapsed && 'Logout'}
             </button>
           ) : (
-            <Link className="side-nav-link" to="/login">Login</Link>
+            <Link className="side-nav-link" to="/login">
+              <i className="fa-solid fa-sign-in-alt"></i> {!collapsed && 'Login'}
+            </Link>
           )}
         </div>
+
+        {/* Sidebar logo at the bottom */}
         <Link to="/" className="logo-link">
           <img src={logo2} alt="logo" className="side-nav-logo" />
         </Link>
       </div>
 
       {/* Overlay */}
-      <div className={`side-nav-overlay ${navOpen ? 'visible' : ''}`} onClick={closeNav}></div>
+      <div className={`side-nav-overlay ${navOpen ? 'visible' : ''}`} onClick={toggleNav}></div>
     </>
   );
 };
